0edd3f7914879a9a73bba83b10d684eec3b6f7f6
Alex Emrie <emrieaj@gmail.com>
2015-02-18 14:43:05 -0700

    Finished quote BugFix and Added User Registration Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "quotes#index"
+
   get "signin" => "sessions#new", as: :signin
   post "signin" => "sessions#create"
   get "signout" => "sessions#destroy", as: :signout
+
+  get '/sign-up' => 'registrations#new', as: :signup
+  post '/sign-up' => 'registrations#create'
+
 end
